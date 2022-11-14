dff957b63b339ca146374b85d4d50b2a1f320abe
ferminotero <ferminotero@gmail.com>
2022-11-14 20:34:07 +0100

    with Melro plots

--- main.py
+++ main.py
@@ -25,7 +25,14 @@ VM = ((Sx-Sy)**2+(Sy-Sz)**2+(Sz-Sx)**2 + 6*(Sxy**2+Sxz**2+Syz**2))/(2*YT**2)-1
 A = (2/math.sqrt(3))*(YT*YC)/(YT+YC)
 B = (1/math.sqrt(3))*(YT-YC)/(YT+YC)
 
-DP = ((Sx-Sy)**2+(Sy-Sz)**2+(Sz-Sx)**2 + 6*(Sxy**2+Sxz**2+Syz**2))/(6*A**2) - (B*(Sx+Sy+Sz)/A)**2-2*B*(Sx+Sy+Sz)/A-1
+J2 = ((Sx-Sy)**2+(Sy-Sz)**2+(Sz-Sx)**2 + 6*(Sxy**2+Sxz**2+Syz**2))/6
+I1 = (Sx+Sy+Sz)
+
+DP = J2/(A**2) - (B*I1/A)**2 - 2*B*I1/A - 1
+
+# ---- Melro Behaviour function ----
+
+Melro = 6*J2 + 2*I1*(YC-YT)-2*YC*YT
 
 # Plot VM for Sx and Sy
 
@@ -55,56 +62,52 @@ DPSxSxy = simplify(DP.subs(replacements))
 replacements = [(Sx,0),(Sy,0),(Sz,0),(Syz,0),(YT,YTv),(YC,YCv)]
 DPSxySxz = simplify(DP.subs(replacements))
 
+
+# Plot Melro for Sx and Sy
+
+YTv = 10
+YCv = 15
+
+replacements = [(Sz,0),(Sxy,0),(Sxz,0),(Syz,0),(YT,YTv),(YC,YCv)]
+MelroSxSy = simplify(Melro.subs(replacements))
+
+replacements = [(Sz,0),(Sy,0),(Sxz,0),(Syz,0),(YT,YTv),(YC,YCv)]
+MelroSxSxy = simplify(Melro.subs(replacements))
+
+replacements = [(Sx,0),(Sy,0),(Sz,0),(Syz,0),(YT,YTv),(YC,YCv)]
+MelroSxySxz = simplify(Melro.subs(replacements))
+
 # Ploting
 
 YTplot = YTv + 0.2*YTv
 YCplot = -(YCv + 0.5*YCv)
+Yshear = YTv*(1/math.sqrt(3)+0.2)
 
-VMSxSyPlot = plot_implicit(Eq(VMSxSy, 0),(Sx, YCplot, YTplot), (Sy, YCplot, YTplot), label='VM', line_color='blue', show=False, legend=True)
-VMSxSxyPlot = plot_implicit(Eq(VMSxSxy, 0),(Sx, YCplot, YTplot), (Sxy, YCplot, YTplot),line_color='blue', show=False, legend=True)
-VMSxySxzPlot = plot_implicit(Eq(VMSxySxz, 0),(Sxy, YCplot, YTplot), (Sxz, YCplot, YTplot),line_color='blue', show=False, legend=True)
+VMSxSyPlot = plot_implicit(Eq(VMSxSy, 0),(Sx, YCplot, YTplot), (Sy, YCplot, YTplot), depth = 1, line_color='blue', show=False)
+VMSxSxyPlot = plot_implicit(Eq(VMSxSxy, 0),(Sx, YCplot, YTplot), (Sxy, -Yshear, Yshear), depth = 1, line_color='blue', show=False)
+VMSxySxzPlot = plot_implicit(Eq(VMSxySxz, 0),(Sxy, -Yshear, Yshear), (Sxz, -Yshear, Yshear), depth = 1, line_color='blue', show=False)
 
-DPSxSyPlot = plot_implicit(Eq(DPSxSy, 0),(Sx, YCplot, YTplot), (Sy, YCplot, YTplot), label='DP', line_color='red', show=False, legend=True)
-DPSxSxyPlot = plot_implicit(Eq(DPSxSxy, 0),(Sx, YCplot, YTplot), (Sxy, YCplot, YTplot), line_color='red', show=False, legend=True)
-DPSxySxzPlot = plot_implicit(Eq(DPSxySxz, 0),(Sxy, YCplot, YTplot), (Sxz, YCplot, YTplot), line_color='red', show=False, legend=True)
+DPSxSyPlot = plot_implicit(Eq(DPSxSy, 0),(Sx, YCplot, YTplot), (Sy, YCplot, YTplot), depth = 1, line_color='red', show=False)
+DPSxSxyPlot = plot_implicit(Eq(DPSxSxy, 0),(Sx, YCplot, YTplot), (Sxy, -Yshear, Yshear), depth = 1, line_color='red', show=False)
+DPSxySxzPlot = plot_implicit(Eq(DPSxySxz, 0),(Sxy, -YTplot, YTplot), (Sxz, -Yshear, Yshear), depth = 1, line_color='red', show=False)
 
+MelroSxSyPlot = plot_implicit(Eq(MelroSxSy, 0),(Sx, YCplot, YTplot), (Sy, YCplot, YTplot), depth = 1, line_color='black', show=False)
+MelroSxSxyPlot = plot_implicit(Eq(MelroSxSxy, 0),(Sx, YCplot, YTplot), (Sxy, -Yshear, Yshear), depth = 1, line_color='black', show=False)
+MelroSxySxzPlot = plot_implicit(Eq(MelroSxySxz, 0),(Sxy, -Yshear, Yshear), (Sxz, -Yshear, Yshear), depth = 1, line_color='black', show=False)
 
 
 VMSxSyPlot.append(DPSxSyPlot[0])
-VMSxSyPlot.show()
+VMSxSyPlot.append(MelroSxSyPlot[0])
+VMSxSyPlot.title = 'VonMises (blue) ; DrukerPrager (red) ; Melro (black)'
+VMSxSyPlot.save('/home/fermin/Documents/Python Projects/YiledCriteriumPlots/Sx_Sy.png')
+
 VMSxSxyPlot.append(DPSxSxyPlot[0])
-VMSxSxyPlot.show()
+VMSxSxyPlot.append(MelroSxSxyPlot[0])
+VMSxSxyPlot.title = 'VonMises (blue) ; DrukerPrager (red) ; Melro (black)'
+VMSxSxyPlot.save('/home/fermin/Documents/Python Projects/YiledCriteriumPlots/Sx_Sxy.png')
+
 VMSxySxzPlot.append(DPSxySxzPlot[0])
-VMSxySxzPlot.show()
+VMSxySxzPlot.append(MelroSxySxzPlot[0])
+VMSxySxzPlot.title = 'VonMises (blue) ; DrukerPrager (red) ; Melro (black)'
+VMSxySxzPlot.save('/home/fermin/Documents/Python Projects/YiledCriteriumPlots/Sxy_Sxz.png')
 
-#fig, (VMSxSyPlot, DPSxSyPlot) = plt.subplots(1, 2, figsize=(8, 4.5))
-#fig, ax1 = plt.subplots()
-#ax1.title.set_text('Von Mises')
-#ax1.set_ylabel("Sy")
-#ax1.set_xlabel("Sx")
-#TBTTauCorrected = [num*factorK for num in TBTTau]
-#ax1.plot(SxPos, SyPosPos, label = "line 1", linestyle=":", color="tab:orange")
-#ax1.plot(SxPos, SyPosNeg, label = "line 1", linestyle=":", color="tab:orange")
-#ax1.plot(SxNeg, SyNegPos, label = "line 1", linestyle=":", color="tab:orange")
-#ax1.plot(SxNeg, SyNegNeg, label = "line 1", linestyle=":", color="tab:orange")
-#ax1.plot(TBTTauCorrected, Zpos, label = "line 1",                color="tab:orange")
-#ax1.plot(PSTau, Zpos, label = "line 1",                color="tab:blue")
-#ax2.plot(integrantTB,          Zpos, label = "TB Original",  linestyle=":", color="tab:orange")
-#ax2.plot(integrantTBcorrected, Zpos, label = "TB Corrected",                color="tab:orange")
-#ax2.plot(integrantPS,          Zpos, label = "PS",                          color="tab:blue")
-#ax1.set_xlabel(r"$\tau$ [MPa]")
-#ax2.set_xlabel("$\Pi$ [J]")
-#ax1.set_xlim([0.0,None])
-#ax1.set_ylim([layers[0][0],layers[-1][1]])
-#ax2.set_xlim([0.0,None])
-#ax2.set_ylim([layers[0][0],layers[-1][1]])
-#ax1.set_yticks(np.arange(layers[0][0],layers[-1][1]+0.001, 1.0))
-#ax2.set_yticks(np.arange(layers[0][0],layers[-1][1]+0.001, 1.0))
-#ax2.axes.yaxis.set_ticklabels([])
-#ax1.title.set_text(r'$\tau^{TB}$(Q) vs $\tau^{PS}$(Q)')
-#ax2.title.set_text(r'$\Pi^{TB}$(Q) vs $\Pi^{PS}$(Q)')
-
-#fig.subplots_adjust(bottom=0.2)
-#labels = ["TB Original","TB Corrected","PS"]
-#fig.legend(labels=labels, loc="lower center", ncol=3)
-#plt.show()
